338cf245a5b376526a820c492179d91e624cf7f3
David Barrios <barriosadavid@gmail.com>
2019-04-29 13:48:54 -0700

    finish with cardcontainer and styling

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return(
+        <div className='cardContent'>
+            <h4>Get Started with React</h4>
+            <p>React makes it painless to cerate interactive UIs. Design simple views for each state in your application.</p>
+            <span>reactjs.org</span>
+        </div>
+    );
+}
+
+export default CardContent;
--- Project-I-Social-Card/social-card/src/index.js
+++ Project-I-Social-Card/social-card/src/index.js
@@ -4,3 +4,9 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+const card = document.querySelector('.cardContainer');
+
+card.addEventListener('click', () => {
+    window.location.href = 'https://www.reactjs.org';
+});
