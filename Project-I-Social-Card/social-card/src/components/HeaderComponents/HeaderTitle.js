28245e237b6e3696a6c1636fea1c444ae707d529
David Barrios <barriosadavid@gmail.com>
2019-04-29 13:09:17 -0700

    added card header and styling to header

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+
+const HeaderTitle = () => {
+    return(
+        <div className='headerTitle'>
+            <div>
+                <h3>Lambda School <span>@LambdaSchool 26 Jan</span></h3>
+            </div>
+            <div>
+                <HeaderContent />
+            </div>
+        </div>
+    );
+}
+
+export default HeaderTitle;
