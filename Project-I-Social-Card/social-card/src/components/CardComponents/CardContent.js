ccecb7a66e43bb84fd43386d394b935fe4a3cd77
BrennanHolm <brennan.mholmich@gmail.com>
2019-04-29 16:20:42 -0500

    Refactor className spelling

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -3,7 +3,7 @@ import './Card.css';
 
 const CardContent = (props) => {
   return (
-    <div className = "card-conent">
+    <div className = "card-content">
       <h3>Get Started With React</h3>
       <p>React makes it painless to create interactive UIs. Design simple views for each state in your application</p>
       <p className = "link"><a href = "https:reactjs.org" target = "_blank">reactjs.org</a></p>
