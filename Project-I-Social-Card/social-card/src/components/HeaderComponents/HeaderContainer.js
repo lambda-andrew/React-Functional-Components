cde6afdf0fe13b068f93aa2e7c53d50018a13b39
BrennanHolm <brennan.mholmich@gmail.com>
2019-04-29 14:53:02 -0500

    Add imports to HeaderContainer

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,5 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderContent from './HeaderContent';
+import HeaderTitle from './HeaderTitle';
