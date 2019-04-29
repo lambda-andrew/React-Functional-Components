679ff59db8909835a2d79f310185d3a58938e240
BrennanHolm <brennan.mholmich@gmail.com>
2019-04-29 15:21:29 -0500

    Add HeaderContainer component
    package-lock is also added this commit

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -3,3 +3,17 @@ import './Header.css';
 import ImageThumbnail from './ImageThumbnail';
 import HeaderContent from './HeaderContent';
 import HeaderTitle from './HeaderTitle';
+
+const HeaderContainer = () => {
+  return (
+    <header>
+    <div className = "thumbnail-title">
+      <HeaderTitle />
+      <ImageThumbnail />
+    </div>
+      <div className = "header-content">
+        <HeaderContent />
+      </div>
+    </header>
+  );
+}
