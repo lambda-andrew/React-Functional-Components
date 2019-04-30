2e73171eed8353a0031845163ae93336b09c1755
Eric Nunn <lowlyherotech@gmail.com>
2019-04-30 18:42:42 -0400

    finished the previous days assignment to understand todays

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,4 +1,15 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
 
-// https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png
+const HeaderContainer = () => {
+    return (
+      <div className="container-header">
+        <ImageThumbnail />
+        <HeaderTitle />
+      </div>
+    );
+  };
+  
+  export default HeaderContainer;
