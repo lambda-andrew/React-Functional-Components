28c0cafbc338f8c7e7c5f27e02bfedc518a54310
efrain <efarinva98@gmail.com>
2019-04-29 20:16:24 -0400

    need to work on css

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -4,8 +4,16 @@ import CardBanner from './CardBanner';
 
 const CardContent = () => {
   return (
-    <div>
+    <div >
     <CardBanner/>
+      <div id = "top">
+
+      <h1>get started with react</h1>
+      <p>
+        react makes it painless to creatte interative uis. design simple views for each state in yout apllication
+      </p>
+      <address>reactttjs.org</address>
+      </div>
     </div>
   )
 };
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -3,7 +3,9 @@ import './Header.css';
 
 const HeaderContent = () => {
   return(
-  <p>words</p>
+  <p>lets learn react by building simple interfaces eith components. dont try to
+    over think it , just keep it simple and have fun. once you feel comfortable using components yout are well on your way to mastering react!
+  </p>
   )
 };
 
